1ac90a638f60727a692e767aaea9de5ac7c0730f
showi <joachim.basmaison@gmail.com>
2017-05-26 01:54:39 +0200

    Config.getpath method replace %module_path% by real path

--- lipicam/config.py
+++ lipicam/config.py
@@ -4,13 +4,23 @@ import lipicam
 
 conf_names = ['lipicam-base.cfg', 'lipicam.cfg']
 
+path2replace = {name: ['%{}%'.format(name), getattr(lipicam, name)] for name in ['module_path']}
+
 class Config(ConfigParser.ConfigParser):
 
     def __init__(self):
         ConfigParser.ConfigParser.__init__(self)
         paths = [P.join(lipicam.config_path, path) for path in conf_names]
-        lipicam.log.debug('path: {}'.format(paths))
         self.read(paths)
+        userconf = P.join(self.getpath('config', 'user'), 'lipicam.cfg')
+        self.read(userconf)
 
+    def getpath(self, section, key):
+        result = self.get(section, key)
+        if result is None:
+            return None
+        for key, tup in path2replace.items():
+            result = result.replace(*tup)
+        return result
 
 config = Config()
